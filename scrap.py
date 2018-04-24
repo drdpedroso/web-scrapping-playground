9e50ef289496c7f4632cb0069f23e3669fbc3f3d
Eduardo <drdpedroso@gmail.com>
2018-04-24 13:41:14 -0300

    Creating a simple scraping script

--- scrap.py
+++ scrap.py
@@ -1,8 +1,18 @@
-from urllib.request import urlopen as uReq
+import re
+from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 
-my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
+my_url = 'https://g1.globo.com/mundo/noticia/van-atropela-pedestres-em-toronto-no-canada.ghtml'
 
-uClient = uReq(my_url)
+uClient = urlopen(my_url)
 page_html = uClient.read()
 uClient.close()
+page_soup = soup(page_html, "html.parser")
+# containers = page_soup.findAll("div", {"class": "item-container"})
+
+# container = containers[0]
+# container.div
+# print(container.a)
+
+x = page_soup.findAll(text=re.compile('Canadá'))
+print(x)
