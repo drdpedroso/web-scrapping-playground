3d2d14378c142e4844581beec74883baad2737f6
Eduardo <drdpedroso@gmail.com>
2018-04-23 22:06:52 -0300

    Adding venv and bs4

---
+++ scrap.py
@@ -0,0 +1,8 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
+
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
